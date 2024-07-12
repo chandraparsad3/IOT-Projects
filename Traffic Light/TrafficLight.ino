b6776a5c52b00677ff677865b76512822c3a67c4
Chandraparsad3 <Chandraparsad02@gmail.com>
2024-07-12 15:18:28 +0630

    Simple Traffic light

---
+++ Traffic Light/TrafficLight.ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(7, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+    digitalWrite(8, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(8, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
